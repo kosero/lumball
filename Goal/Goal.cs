05e8edce98d9176d7d2eba9f6256a2e88469acc2
kosero <kosero@tuta.io>
2025-10-22 02:57:13 +0300

    feat: I added the football goal and event

---
+++ Goal/Goal.cs
@@ -0,0 +1,46 @@
+using Godot;
+using System;
+
+public partial class Goal : Area2D
+{
+	[Export] public bool isRed = false;
+
+	private Sprite2D redSkin;
+	private Sprite2D blueSkin;
+
+	private Area2D area;
+	[Signal] public delegate void LumballInGoalEventHandler();
+
+	public override void _Ready()
+	{
+		redSkin = GetNode<Sprite2D>("Skins/Red");
+		blueSkin = GetNode<Sprite2D>("Skins/Blue");
+
+		if (isRed)
+		{
+			redSkin.Visible = true;
+			blueSkin.Visible = false;
+		}
+		else
+		{
+			redSkin.Visible = false;
+			blueSkin.Visible = true;
+		}
+
+		area = GetNode<Area2D>("%Area2D");
+		area.BodyEntered += OnBodyEntered;
+	}
+
+	private void OnBodyEntered(Node body)
+	{
+		if (body is TileMapLayer)
+			return;
+
+		if (body is Lumball)
+		{
+			body.QueueFree();
+			EmitSignal("LumballInGoal");
+		}
+	}
+
+}
